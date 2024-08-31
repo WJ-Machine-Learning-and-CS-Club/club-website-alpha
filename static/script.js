8c3f205e7d0f0ffe8518948c6a7db89eeb951ea1
JYGithub <93101404+zyx5202@users.noreply.github.com>
2024-08-30 20:02:34 -0400

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,38 @@
+
+$('#prevbut').bind('click', function(e) {
+    $('#myCarousel').carousel('prev');
+  });
+  $('#nextbut').bind('click', function(e) {
+    $('#myCarousel').carousel('next');
+  });
+  //search bar                                     
+  let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    let i;     
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+  //searching
+  const club_titles = [];
+  const club_images = ["static/images/latinDance.jpg", "static/images/ski.jpg", "static/images/turkish.jpg", "static/images/untitled.jpg"];
+  
+  function on_search() {
+  
+  }
+  
+  function edge() {
+    for (let i = 0; i < 200; i++) {
+      club_titles.push(document.getElementById("club" + i).outerHTML);
+  
+    }
+  
+  
+  }
