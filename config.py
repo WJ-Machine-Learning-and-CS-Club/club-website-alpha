b288e8b9b7cf5e0d370336b32feead9f7a316066
JYGithub <93101404+zyx5202@users.noreply.github.com>
2025-02-04 11:26:39 -0500

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,11 @@ REFERENCE_FEATURED_CLUBS_INFO = os.path.join(UPLOAD_FOLDER_DATA, 'featured_clubs
 ALLOWED_EXTENSIONS = {'csv'}
 
 # Image configs
-base_url = "https://raw.githubusercontent.com/WJ-Machine-Learning-and-CS-Club/wjclubs_2024_fixed_image_names/main/updated_images/"
+#base_url = "https://raw.githubusercontent.com/WJ-Machine-Learning-and-CS-Club/wjclubs_2024_fixed_image_names/main/updated_images/"
+base_url ="https://raw.githubusercontent.com/WJ-Machine-Learning-and-CS-Club/club-website-alpha/main/static/images/"
 extensions = ['.jpg', '.png', '.jpeg', '.webp', '.svg']
 default_image=os.path.join('images', 'unknown.png')
 default_img = "unknown.png"
 featured_header="Featured "
-expected_columns_allclubs = ["Club Name", "Purpose", "Meeting Days", "Meeting Times", "Meeting Location", "Club Meeting Frequency", "Social Media Handles"]
+expected_columns_allclubs = ["Club Name", "Sponsor email address","Purpose", "Select all the days of the week that your club meets", "When does your club meet other than lunch or after school? (If you do not please put N/A)", "Club Meeting Location", "Club Meeting Frequency", "Social Media Handles (optional)"]
 expected_columns_featured = ["Club Name", "Description"]
